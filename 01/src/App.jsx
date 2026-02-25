217308772372255c0f06e4009b7af69e7a285d8f
Akhil sambasivan <akhilsam7722@gmail.com>
2026-02-25 18:54:43 +0530

    folder-1

---
+++ 01/src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.scss"
+
+const App = () => {
+  return (
+    <main>
+      <h1>Introduction about React</h1>
+
+      <div className="con1">
+        <ul>
+          <li>Learned creating react project with vite</li>
+          <li>Included scss in the file</li>
+        </ul>
+      </div>
+    </main>
+  )
+}
+
+export default App
